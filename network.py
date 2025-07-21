d106a85c0feb7c70fd4d0c3180ee8b32caa43ad6
muffindud <ccatlabuga2002@gmail.com>
2025-07-21 15:45:00 +0300

    set routes to return nothing

--- network.py
+++ network.py
@@ -1,8 +1,7 @@
 from os import getenv
+from random import randint, sample
 
-import random
-
-from quart import Quart, request, jsonify
+from quart import Quart, request
 from httpx import AsyncClient
 
 
@@ -29,20 +28,23 @@ def link(module_id: str, neighbor_id: str) -> None:
 
 
 def create_random_links(module_id: str) -> None:
+    if len(network) == 1:
+        return
+
     if BRANCHING_FACTOR[0] < len(network):
         for node in network.keys():
             link(module_id, node)
 
     elif BRANCHING_FACTOR[1] < len(network):
-        branch_count = random.randint(BRANCHING_FACTOR[0], len(network))
-        nodes = random.sample(list(network.keys()), branch_count)
+        branch_count = randint(BRANCHING_FACTOR[0], len(network))
+        nodes = sample(list(network.keys()), branch_count)
 
         for node in nodes:
             link(module_id, node)
 
     else:
-        branch_count = random.randint(*BRANCHING_FACTOR)
-        nodes = random.sample(list(network.keys()), branch_count)
+        branch_count = randint(*BRANCHING_FACTOR)
+        nodes = sample(list(network.keys()), branch_count)
 
         for node in nodes:
             link(module_id, node)
@@ -50,34 +52,44 @@ def create_random_links(module_id: str) -> None:
 
 @app.route("/subscribe/worker/<string:module_id>", methods=["POST"])
 async def subscribe_worker(module_id: str):
+    print(f"Received worker subscribe {module_id} {request.remote_addr}")
+
     network[module_id] = set()
     module_id_map[module_id] = request.remote_addr
     create_random_links(module_id)
-    return jsonify({"status": "subscribed", "module_id": module_id}), 200
+
+    return "", 204
 
 
 @app.route("/subscribe/manager/<string:module_id>", methods=["POST"])
 async def subscribe_manager(module_id: str):
+    print(f"Received manager subscribe {module_id} {request.remote_addr}")
+
     network[module_id] = set()
     module_id_map[module_id] = request.remote_addr
     global manager_id
     manager_id = module_id
-    return jsonify({"status": "subscribed", "module_id": module_id}), 200
+    create_random_links(module_id)
+
+    return "", 204
 
 
-@app.route("/send/<string:module_id>", method=["POST"])
+@app.route("/send/<string:module_id>", methods=["POST"])
 async def send(module_id: str):
     neighbors = network.get(module_id, [])
     body = await request.get_data()
+
+    print(f"Received packet from {request.remote_addr}")
 
     for neighbor in neighbors:
         if neighbor in module_id_map.keys():
             neighbor_url = f"http://{module_id_map[neighbor]}:{MODULE_PORT}/receive"
 
             async with AsyncClient() as client:
+                print(f"Redirected packet to {neighbor_url}")
                 await client.post(neighbor_url, content=body)
 
-    return jsonify({"status": "message sent", "module_id": module_id}), 200
+    return "", 204
 
 
 if __name__ == "__main__":
